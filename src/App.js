4a9f803682ce43aee248d1ab2245046c86292b65
john-reiner <johnreiner1990@gmail.com>
2020-10-21 12:26:31 -0400

    added hooks and tested trainer name response

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [trainerName, setTrainerName] = useState('')
+  const [trainerpin, setTrainerpin] = useState('')
+
+  const handleChange = (e) => setTrainerName(e.target.value)
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    alert(trainerName)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Pokedex</h1>
+      <form onSubmit={handleSubmit} >
+        <input type="text" name="name" placeholder="Name" onChange={handleChange}></input>
+        <input type="submit" value="Submit" />
+      </form>
     </div>
   );
 }
